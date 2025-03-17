b8e4e0f0603103774064460ce4872e02c7b06f23
doudou <doudoumao1031@gmail.com>
2025-03-17 21:03:40 +0800

    feat: app layout update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
+import LeftBar from "@/components/LeftBar";
 import "./globals.css";
+import RightBar from "@/components/RightBar";
 
 export default function RootLayout({
   children,
@@ -7,7 +9,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {/* sm 640
+        md 768
+        lg 1024
+        xl 1280
+        2xl 1536 */}
+        <div className="flex justify-between bg-red-300 sm:bg-blue-300 md:bg-green-300 lg:bg-yellow-300 xl:bg-orange-300 2xl:bg-purple-300">
+          <LeftBar />
+          <div className="flex-1">{children}</div>
+          <RightBar />
+        </div>
+      </body>
     </html>
   );
 }
