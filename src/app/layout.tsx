ca75c52fdce4fb14fd5a816896d0f3770e65fa9f
doudou <doudoumao1031@gmail.com>
2025-03-19 21:24:19 +0800

    feat: Tailwind Custom Responsive Layout Screens update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,14 +10,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        {/* sm 640
-        md 768
-        lg 1024
-        xl 1280
-        2xl 1536 */}
-        <div className="flex justify-between bg-red-300 sm:bg-blue-300 md:bg-green-300 lg:bg-yellow-300 xl:bg-orange-300 2xl:bg-purple-300">
+        <div className="max-w-screen-md lg:max-w-screen-lg mx-auto flex justify-between">
           <LeftBar />
-          <div className="flex-1">{children}</div>
+          <div className="">{children}</div>
           <RightBar />
         </div>
       </body>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,21 @@ export default {
   ],
   theme: {
     extend: {
+      screens: {
+        // xsm: "320px",
+        // sm: "640px",
+        // md: "768px",
+        // lg: "1024px",
+        // xl: "1280px",
+        // "2xl": "1536px",
+        // "3xl": "1920px",
+        xsm: "500px",
+        sm: "600px",
+        md: "690px",
+        lg: "988px",
+        xl: "1078px",
+        xxl: "1265px"
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
