09755b767ffb248b0c57377c3fa47395d000f391
doudou <doudoumao1031@gmail.com>
2025-03-20 14:14:11 +0800

    feat: Tailwind Custom Responsive Layout Screens complete

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,10 +10,24 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <div className="max-w-screen-md lg:max-w-screen-lg mx-auto flex justify-between">
-          <LeftBar />
-          <div className="">{children}</div>
-          <RightBar />
+        <div 
+          className="
+            max-w-screen-sm 
+            lg:max-w-screen-lg 
+            xl:max-w-screen-xl
+            xxl:max-w-screen-xxl
+            mx-auto 
+            flex 
+            justify-between
+          "
+        >
+          <div className="px-2 xsm:px-4 bg-red-200">
+            <LeftBar />
+          </div>
+          <div className="flex-1 lg:min-w-[600px] border-x-[1px] border-borderGray">{children}</div>
+          <div className="hidden lg:flex ml-4 md:ml-8 flex-1 bg-blue-200">
+            <RightBar />
+          </div>
         </div>
       </body>
     </html>
